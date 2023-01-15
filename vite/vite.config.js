2f959bf0af97e5a6cbf7bf4abe94c81ecd111f57
Mathews A <mathews.mw@gmail.com>
2023-01-14 21:41:15 -0400

    vite config

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [react()],
+	resolve: {
+		alias: {
+			process: 'process/browser',
+			stream: 'stream-browserify',
+			zlib: 'browserify-zlib',
+			util: 'util',
+		},
+	},
 })
